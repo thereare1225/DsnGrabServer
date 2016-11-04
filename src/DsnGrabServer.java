dde7d673eeb13f78d343997e027e9ec02a5db32f
thereare1225 <383109260@qq.com>
2016-11-04 11:40:53 +0800

    打开log

--- src/DsnGrabServer.java
+++ src/DsnGrabServer.java
@@ -513,23 +513,23 @@ public class DsnGrabServer {
      
     public static void main(String [] args) throws IOException{
     	//重定向输出
-//    	try {  
-//	    	//生成路径  
-//	    	File dir = new File("log");  
-//	        if (dir.exists()) {   
-//	        } 
-//	        else {
-//	        	dir.mkdirs();
-//	        }
-//	         
-//	        //把输出重定向到文件
-//	    	SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");//设置日期格式
-//	    	PrintStream ps=new PrintStream("log/" + df.format(new Date()) + ".txt");  
-//	    	System.setOut(ps);
-//	    	System.setErr(ps);
-//	    } catch (FileNotFoundException e) {  
-//	    	e.printStackTrace();
-//		} 
+    	try {  
+	    	//生成路径  
+	    	File dir = new File("log");  
+	        if (dir.exists()) {   
+	        } 
+	        else {
+	        	dir.mkdirs();
+	        }
+	         
+	        //把输出重定向到文件
+	    	SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");//设置日期格式
+	    	PrintStream ps=new PrintStream("log/" + df.format(new Date()) + ".txt");  
+	    	System.setOut(ps);
+	    	System.setErr(ps);
+	    } catch (FileNotFoundException e) {  
+	    	e.printStackTrace();
+		} 
     	new DsnGrabServer().init();
     }
  
